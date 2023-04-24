fc08fdbaf6b717108f5ef010fc23520963f8daad
henry4912 <henryha4912@gmail.com>
2023-04-23 21:12:26 -0700

    Deleted redundant file write

--- scraper.py
+++ scraper.py
@@ -37,7 +37,6 @@ def extract_next_links(url, resp):
     # needs data structure to hold the hash values
 
         soup = BeautifulSoup(resp.raw_response.content, "html.parser")
-        output.write(soup.text)
         tags = soup.find_all('a')
         links = []
         for link in tags:
